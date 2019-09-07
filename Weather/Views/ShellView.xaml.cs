462110aaf8f0324a51ec02dcec0a47bac8352e51
Asklepius <andreyrv91@gmail.com>
2019-09-07 14:16:45 +0300

    Построена архитектура с использованием Caliburn.micro

--- Weather/Views/ShellView.xaml.cs
+++ Weather/Views/ShellView.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Weather
+namespace Weather.Views
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для ShellView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ShellView : Window
     {
-        public MainWindow()
+        public ShellView()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
